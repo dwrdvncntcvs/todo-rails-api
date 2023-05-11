b207f0a58abe5ece9f79d0e4d9b775756831eae8
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-05-11 20:31:00 +0800

    Create auth login method implementation

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,18 @@
+require_relative '../helpers/jwt_helper'
+
 class AuthController < ApplicationController
-  def login; end
+  before_action :validate_user, only: [:login]
+
+  def login
+    @user = User.find_by(username: login_params[:username])
+
+    if @user&.authenticate(login_params[:password])
+      token = JsonWebToken.jwt_encode({ id: @user.id, username: @user.username })
+      render json: { token: }, status: :ok
+    else
+      render json: { errors: "Invalid Username and Password" }, status: :bad_request
+    end
+  end
 
   def register
     @user = User.new(auth_params)
@@ -15,7 +28,18 @@ class AuthController < ApplicationController
 
   private
 
+  def validate_user
+    return unless login_params[:username].nil? || login_params[:password].nil?
+
+    render json: { message: 'Invalid Username and Password' },
+           status: :bad_request
+  end
+
   def auth_params
     params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
   end
 end
